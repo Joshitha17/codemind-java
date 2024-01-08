758383b7eea9aa9f2786a10a6feeab9ba170c9e0
Mantharapu Joshitha Maha Lakshmi <119913478+Joshitha17@users.noreply.github.com>
2024-01-08 23:15:38 +0530

    import java.util.Scanner; public class Noodles{     public static void main (String [] args)     {         Scanner sc=new Scanner(System.in);         int a=sc.nextInt();         int b=sc.nextInt();         int x=b/a;         System.out.println(x);     } }

---
+++ Required_Points:.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class Noodles{
+    public static void main (String [] args)
+    {
+        Scanner sc=new Scanner(System.in);
+        int a=sc.nextInt();
+        int b=sc.nextInt();
+        int x=b/a;
+        System.out.println(x);
+    }
+}
