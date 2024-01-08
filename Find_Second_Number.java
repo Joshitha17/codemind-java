f08ed9258af78a8ef000725f6046be089e58dd63
Mantharapu Joshitha Maha Lakshmi <119913478+Joshitha17@users.noreply.github.com>
2024-01-08 23:10:29 +0530

    import java.util.Scanner; public class Bridge{     public static void main(String [] args)     {         Scanner sc=new Scanner(System.in);         int x=sc.nextInt();         int y=sc.nextInt();         int a=x*2-y;         System.out.println(a);     } }

---
+++ Find_Second_Number.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class Bridge{
+    public static void main(String [] args)
+    {
+        Scanner sc=new Scanner(System.in);
+        int x=sc.nextInt();
+        int y=sc.nextInt();
+        int a=x*2-y;
+        System.out.println(a);
+    }
+}
