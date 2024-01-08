764ec2ca64995a9ad6ad13d335bcf5e21047c8cb
Mantharapu Joshitha Maha Lakshmi <119913478+Joshitha17@users.noreply.github.com>
2024-01-08 23:29:40 +0530

    import java.util.Scanner; public class KingTours{     public static void main(String [] args)     {         Scanner sc=new Scanner(System.in);         int x=sc.nextInt();         int y=sc.nextInt();         int a=(x*5)+(y*7);         System.out.println(a);     } }

---
+++ King_Tours_.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class KingTours{
+    public static void main(String [] args)
+    {
+        Scanner sc=new Scanner(System.in);
+        int x=sc.nextInt();
+        int y=sc.nextInt();
+        int a=(x*5)+(y*7);
+        System.out.println(a);
+    }
+}
