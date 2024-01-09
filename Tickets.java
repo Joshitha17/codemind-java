ce844f99091aff336ddd901a12398e0c37895847
Mantharapu Joshitha Maha Lakshmi <119913478+Joshitha17@users.noreply.github.com>
2024-01-09 23:38:19 +0530

    import java.util.Scanner; public class Solution{     public static void main(String [] args)     {         Scanner sc=new Scanner(System.in);         int a=sc.nextInt();         if((a*4)<=1000)         {             System.out.println("YES");         }         else         {             System.out.println("NO");         }     } }

---
+++ Tickets.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class Solution{
+    public static void main(String [] args)
+    {
+        Scanner sc=new Scanner(System.in);
+        int a=sc.nextInt();
+        if((a*4)<=1000)
+        {
+            System.out.println("YES");
+        }
+        else
+        {
+            System.out.println("NO");
+        }
+    }
+}
