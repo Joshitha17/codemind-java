c8446688a6c219152c9ac6bcb1e2479bb92e84bf
Mantharapu Joshitha Maha Lakshmi <119913478+Joshitha17@users.noreply.github.com>
2024-01-10 22:53:07 +0530

    import java.util.Scanner; public class Solution{     public static void main(String [] args)     {         Scanner sc=new Scanner (System.in);         int a=sc.nextInt();         int i;         for(i=a;i>=1;i--)         {             System.out.print(i+" ");         }     } }

---
+++ N_to_1.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class Solution{
+    public static void main(String [] args)
+    {
+        Scanner sc=new Scanner (System.in);
+        int a=sc.nextInt();
+        int i;
+        for(i=a;i>=1;i--)
+        {
+            System.out.print(i+" ");
+        }
+    }
+}
