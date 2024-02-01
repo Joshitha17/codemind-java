8efd97de9b9bc42df478e5c3616241a95584e13e
Mantharapu Joshitha Maha Lakshmi <119913478+Joshitha17@users.noreply.github.com>
2024-02-01 11:18:54 +0530

    import java.util.*; public class Main{     public static void main(String[] args)     {         Scanner sc=new Scanner(System.in);         int n=sc.nextInt();         for(int i=0;i<n;i++)         {             int a=sc.nextInt();             int b=sc.nextInt();             if((b%a==0) || (a<=(b/2)))             {                 System.out.println("YES");             }             else             {                 System.out.println("NO");             }         }     } }

--- Can_be_a_factor?.java
+++ Can_be_a_factor?.java
@@ -1,20 +1,21 @@
-import java.util.*; 
-public class Factor { 
-    public static void main(String args[]) { 
-        Scanner sc=new Scanner(System.in); 
-        int t=sc.nextInt(); 
-        while(t-->0) { 
-            int a,b,z;
-            a=sc.nextInt();
-            b=sc.nextInt();
-            z=Math.abs(a-b);
-            if(z>=a||a==b) 
-            System.out.println("YES"); 
-            else 
-            System.out.println("NO"); 
-            
-        } 
-        
-    } 
-    
+import java.util.*;
+public class Main{
+    public static void main(String[] args)
+    {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        for(int i=0;i<n;i++)
+        {
+            int a=sc.nextInt();
+            int b=sc.nextInt();
+            if((b%a==0) || (a<=(b/2)))
+            {
+                System.out.println("YES");
+            }
+            else
+            {
+                System.out.println("NO");
+            }
+        }
+    }
 }
